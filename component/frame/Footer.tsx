ef4fde5b4a68ad81ff52b621881780fb9bc70cd5
yyyoichi <origamiichiro@gmail.com>
2022-05-26 13:26:34 +0900

    footer

--- component/frame/Footer.tsx
+++ component/frame/Footer.tsx
@@ -1,6 +1,9 @@
 import styles from "../../styles/Fram.module.css";
 export default function Footer() {
     return (
-        <div className={styles.footer}>Footer</div>
+        <div className={styles.footer}>
+            <div className={styles.footTitle}>明天</div>
+            <div className={styles.footSub}>MYOTEN</div>
+        </div>
     )
 }

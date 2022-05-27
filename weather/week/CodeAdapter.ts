08340d5a66c59d2ef9b4e71b5d2929426a62ac24
yyyoichi <origamiichiro@gmail.com>
2022-05-27 12:54:35 +0900

    codeAdepter実装

--- weather/week/CodeAdapter.ts
+++ weather/week/CodeAdapter.ts
@@ -1,6 +1,18 @@
 export type AboutJPWeather = "晴" | "曇" | "雨" | "雪" | "霧"
-export default interface CodeAdapter {
-    code: number | string;
-    aboutJPWeather: AboutJPWeather[];
-    _getAboutJPWeather: () => AboutJPWeather[];
+export interface CodeAdp {
+    // code: number | string;
+    // aboutJPWeather: AboutJPWeather[];
+    getAboutJPWeather: () => AboutJPWeather[];
+}
+
+export default abstract class CodeAdapter {
+    protected aboutJPWeather: AboutJPWeather[] = []
+    public getAboutJPWeather(): AboutJPWeather[] {
+        if (this.aboutJPWeather.length > 0) {
+            return this.aboutJPWeather
+        }
+        this.aboutJPWeather = this._getAboutJPWeather()
+        return this.aboutJPWeather
+    }
+    protected abstract _getAboutJPWeather(): AboutJPWeather[];
 }

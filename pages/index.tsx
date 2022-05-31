5055035824792878d6553a58c0a8a5d17e8fc77f
yyyoichi <origamiichiro@gmail.com>
2022-05-31 11:28:02 +0900

    _document追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,7 @@
 import FadeIn from '../component/anime/FadeIn';
 import Top from '../main/Top';
-// import getWeathreMap from '../logic/weatherMap';
 import Wrapper from '../component/frame/Wrapper';
-import Head from 'next/head';
 import useWeather from '../weather/useWeather';
-import puppeteer from "puppeteer";
 import getWeatherMap from '../weather/getWeatherMap';
 
 
@@ -16,11 +13,6 @@ export default function Home({ imgSrc }: Props) {
   const { weather } = useWeather()
   return (
     <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Train+One&display=swap" rel="stylesheet" />
-      </Head>
       <Wrapper>
         <FadeIn toggle={!weather.isEmply()} display={true}>
           <Top imgSrc={imgSrc} />

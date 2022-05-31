e97ce5355659f61746ef4e58cc1dd98c596f12be
yyyoichi <origamiichiro@gmail.com>
2022-05-31 11:09:33 +0900

    図をstaticprops内で取得

--- pages/api/weatherimg.js
+++ pages/api/weatherimg.js
@@ -1,13 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+import puppeteer from "puppeteer";
 export default async function handler(req, res) {
-  const url = 'https://api.open-meteo.com/v1/forecast?latitude=35.6785&longitude=139.6823&daily=weathercode,temperature_2m_max,temperature_2m_min&current_weather=true&timezone=Asia%2FTokyo';
-  const response = await fetch(url);
-  if (response.status !== 200) {
-    return {
-      data: 'faliture'
-    }
-  }
-  const data = await response.json();
-  res.status(200).json(data);
+  const url = "https://www.jma.go.jp/bosai/weather_map/";
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.goto(url, { waitUntil: 'networkidle2' })
+  const img = await page.$('.weather-map-viewarea img')
+  const src = await (await img?.getProperty('src'))
+  const imgSrc = await src?.jsonValue()
+  console.log(imgSrc)
+  res.status(200).json(imgSrc);
 }

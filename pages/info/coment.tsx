51bb21b5a92262c198c32ff7ca8230440e8bb7f5
yyyoichi <origamiichiro@gmail.com>
2022-05-31 16:50:13 +0900

    ヘッド追加

--- pages/info/coment.tsx
+++ pages/info/coment.tsx
@@ -1,9 +1,16 @@
+import Head from 'next/head'
 import Wrapper from "../../components/frame/Wrapper"
 
 export default function Home() {
     return (
-        <Wrapper>
-        </Wrapper>
+        <>
+            <Head>
+                <title>明天 -MYOTEN- / 利用規約</title>
+                <meta name="description" content="天気図から明日の天気を当てる暇アプリ。明日の東京の天気は...？" />
+            </Head>
+            <Wrapper>
+            </Wrapper>
+        </>
     )
 }
 

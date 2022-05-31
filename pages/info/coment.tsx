6f9cbbbe2a2aa6fde65740a8ce8fac24f3e32e75
yyyoichi <origamiichiro@gmail.com>
2022-05-31 17:44:12 +0900

    リンク色変更

--- pages/info/coment.tsx
+++ pages/info/coment.tsx
@@ -13,8 +13,8 @@ export default function Home() {
                 <Main>
                     <div>
                         <h2>明天 -MYOTEN- ついて</h2>
-                        <p>明天 -MYOTEN- (以下、当サイト)は、<a href="https://www.jma.go.jp/" target="_blank" rel="noopener noreferrer">気象庁ホームページ</a>で公開されている天気図、天気予報（以下、コンテンツ）を用いています。</p>
-                        <p>コンテンツ利用は、気象庁ホームページで公開されてる <a href="https://www.jma.go.jp/jma/kishou/info/coment.html" target="_blank" rel="noopener noreferrer">利用規約</a>に基づいています。</p>
+                        <p>明天 -MYOTEN- (以下、当サイト)は、<a style={{color: "blue"}} href="https://www.jma.go.jp/" target="_blank" rel="noopener noreferrer">気象庁ホームページ</a>で公開されている天気図、天気予報（以下、コンテンツ）を用いています。</p>
+                        <p>コンテンツ利用は、気象庁ホームページで公開されてる <a style={{color: "blue"}} href="https://www.jma.go.jp/jma/kishou/info/coment.html" target="_blank" rel="noopener noreferrer">利用規約</a>に基づいています。</p>
                         <p>また、当サイトは気象を予報、予測するものではありません。</p>
                     </div>
                     <div>

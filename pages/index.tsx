13729d004b97fd9a7d5ec0d082aee38e3aa1c630
yyyoichi <origamiichiro@gmail.com>
2022-05-31 11:12:58 +0900

    no-sandbox

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,7 +31,9 @@ export default function Home({ imgSrc }: Props) {
 
 export async function getStaticProps() {
   const url = "https://www.jma.go.jp/bosai/weather_map/";
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  })
   const page = await browser.newPage()
   await page.goto(url, { waitUntil: 'networkidle2' })
   const img = await page.$('.weather-map-viewarea img')

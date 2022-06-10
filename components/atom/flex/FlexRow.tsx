550334b4d02226cb21adffc5680c756e855dc0bf
yyyoichi <origamiichiro@gmail.com>
2022-06-10 21:46:00 +0900

    css呼び出し修正

--- components/atom/flex/FlexRow.tsx
+++ components/atom/flex/FlexRow.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-import styles from "../../styles/Container.module.css";
+import styles from "../../../styles/Container.module.css";
 export default function FlexRow({ children }: { children: ReactNode }) {
     return <div className={styles.flexRow}>
         {children}
